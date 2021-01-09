1d7ff6d17038cf018091ba9e605647ea5530581b
sbathke <sbathke@users.noreply.github.com>
2021-01-09 15:29:50 +0100

    Update RingLight.h
    changed visibility of method setUp() to private

--- RingLight.h
+++ RingLight.h
@@ -6,17 +6,17 @@
 class RingLight
 {
   public:
-  	RingLight();
-  	RingLight(uint8_t pin);
+    RingLight();
+    RingLight(uint8_t pin);
     RingLight(uint8_t pin, uint8_t brightness);
     void RingLight::startCountDown(uint16_t timeInSec);
     void RingLight::stopCountDown(); 
     void RingLight::operate();
-    void RingLight::setUp(uint8_t pin, uint8_t brightness);
   private:
   	void RingLight::blink(uint32_t c);
     void RingLight::setColorClockwise(uint32_t c, uint8_t numberOfLeds);
-    void RingLight::drawLedRing();
+    void RingLight::drawLedRing();  
+    void RingLight::setUp(uint8_t pin, uint8_t brightness);
 };
 
 #endif
